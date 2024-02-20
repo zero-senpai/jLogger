4caa8db314aebad9dff2e01163d98b933ac125da
Jake Brunton <106359025+zero-senpai@users.noreply.github.com>
2024-02-20 14:14:00 -0500

    Create DBMain.java
    Initial push for the Database Connector for MySQL

---
+++ src/main/java/com/jLogger/mvn/DBMain.java
@@ -0,0 +1,14 @@
+package com.jLogger.mvn;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * The main DB Connector class and fields.
+ * @Usage: Connects to our MySQL Database.
+ * #TODO: Add implementation to allow user to define their own MySQL server.
+ **/
+
+
+public class DBMain {
+
+}
