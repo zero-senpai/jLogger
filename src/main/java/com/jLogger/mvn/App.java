f6f6b615915c52173758d4ebdfe729d9cfa6dfbd
Jake Brunton <106359025+zero-senpai@users.noreply.github.com>
2024-02-20 13:58:42 -0500

    Initial commit

---
+++ src/main/java/com/jLogger/mvn/App.java
@@ -0,0 +1,7 @@
+package com.jLogger.mvn;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/main/java/com/jLogger/mvn/User.java
@@ -0,0 +1,53 @@
+package com.jLogger.mvn;
+import org.mindrot.jbcrypt.BCrypt;
+
+public class User {
+	private String username;
+	private String password;
+	private int age;
+	private long accountAge;
+	
+	
+	//Default constructor
+	public User() {
+		this.username = "None";
+		this.password = "empty";
+		this.age = 0;
+		this.accountAge = 0;
+	}
+	
+	//Constructor Overload - Information
+	public User(String name, String pwd, int ageYears) {
+		this.username = name;
+		this.password = hashPassword(pwd);
+		this.age = ageYears;
+	}
+	
+	private String hashPassword(String pwd) {
+		return BCrypt.hashpw(pwd, BCrypt.gensalt());
+	}
+	
+	public boolean checkPassword(String pwd) {
+		return BCrypt.checkpw(pwd, password);
+	}
+	
+	//Getters and Setters
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String pwd) {
+		this.password = pwd;
+	}
+	
+	
+	
+}
