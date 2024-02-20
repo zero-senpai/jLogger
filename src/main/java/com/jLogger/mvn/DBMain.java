140a487883bd9772f6832c4f935a6a24fe9cf89c
Jake Brunton <106359025+zero-senpai@users.noreply.github.com>
2024-02-20 14:18:19 -0500

    Update DBMain.java
    Add try/catch for establishing init connection

--- src/main/java/com/jLogger/mvn/DBMain.java
+++ src/main/java/com/jLogger/mvn/DBMain.java
@@ -1,6 +1,7 @@
 package com.jLogger.mvn;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.DriverManager;
 
 /**
  * The main DB Connector class and fields.
@@ -11,4 +12,27 @@ import java.sql.SQLException;
 
 public class DBMain {
 
+	private static final String JDBC_URL = "jdbc:mysql://localhost:3306/mydb";
+	private static final String USERNAME = "admin";
+	private static final String PASSWORD = "root";
+	
+	public static void main(String[] args) {
+		
+		//Create connection
+		try (Connection conn = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD)) {
+			if (conn != null) {
+				System.out.println("Connected to MySQL DB");
+			} else {
+				System.out.println("Could not connect to MySQL!");
+			}
+		} catch (SQLException e) {
+			System.err.println("Connection failed!!!!");
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+	
 }
