2da151dd21e7d36b362edae5d583c64e94e3de0d
Jake Brunton <106359025+zero-senpai@users.noreply.github.com>
2024-02-20 14:33:24 -0500

    Update DBConfig.java

--- src/main/java/com/jLogger/mvn/DBConfig.java
+++ src/main/java/com/jLogger/mvn/DBConfig.java
@@ -1,12 +1,8 @@
 package com.jLogger.mvn;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.DriverManager;
-
 public abstract class DBConfig {
 
-	private static final String JDBC_URL = "jdbc:mysql://localhost:3306/mydb";
-	private static final String USERNAME = "admin";
-	private static final String PASSWORD = "root";
+	public static final String JDBC_URL = "jdbc:mysql://localhost:3306/mydb";
+	public static final String USERNAME = "admin";
+	public static final String PASSWORD = "root";
 	
 }
