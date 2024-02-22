ac74e7849c36f0f18dbd2f719e12d4dbe7f59485
Jake Brunton <106359025+zero-senpai@users.noreply.github.com>
2024-02-22 10:06:57 -0500

    Create LoginFormController.java

---
+++ src/main/java/com/jLogger/mvn/controllers/LoginFormController.java
@@ -0,0 +1,12 @@
+package com.jLogger.mvn.controllers;
+import com.jLogger.mvn.views.*;
+import com.jLogger.mvn.models.*;
+
+public class LoginFormController {
+
+	private LoginFormView view;
+	private User userModel;
+	
+	
+	
+}
